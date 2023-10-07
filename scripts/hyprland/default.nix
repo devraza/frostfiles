0aaeed3869a9b9e53c44c0d54949a32f833bbb4d
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2023-10-07 20:36:03 +0100

    refactor: scripts

--- scripts/hyprland/default.nix
+++ scripts/hyprland/default.nix
@@ -7,11 +7,39 @@ let
       stdbuf -o0 awk -F '>>|,' -e '/^workspace>>/ {print $2}' -e '/^focusedmon>>/ {print $3}'
   '';
 
+  hyprmpc = pkgs.writeShellScriptBin "hyprmpc" ''
+    #!/bin/bash
+
+    ## Play the previous song in the playlist
+    previous_song() {
+  	  mpc -q prev && notify-send --expire-time=2000 "Now Playing" "<span color='#78b9c4'>$(~/.config/eww/scripts/music --artist)</span> \n$(~/.config/eww/scripts/music --song)"
+    }
+    ## Play the next song in the playlist
+    next_song() {
+  	  mpc -q next && notify-send --expire-time=2000 "Now Playing" "<span color='#78b9c4'>$(~/.config/eww/scripts/music --artist)</span> \n$(~/.config/eww/scripts/music --song)"
+    }
+    ## Pause the song
+    toggle() {
+	    mpc -q toggle && notify-send --expire-time=1000 "MPD" "<span color='#f06969'>Toggled Playback!</span>"
+    }
+
+    ## Execute accordingly
+    if [[ "$1" == "--previous" ]]; then
+	    previous_song
+    elif [[ "$1" == "--next" ]]; then
+	    next_song
+    elif [[ "$1" == "--toggle" ]]; then
+	    toggle
+    fi
+  '';
+
 in {
   home.packages = [
     active-workspace
-    # Script dependencies
+    hyprmpc
+    # Script(s) dependencies
     pkgs.socat
     pkgs.jq
+    pkgs.mpc-cli
   ];
 }
