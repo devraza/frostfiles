d44253526d2a89b7c922a479d914173dad2bd840
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2023-10-12 21:18:29 +0100

    feat: printing

--- shared.nix
+++ shared.nix
@@ -20,10 +20,19 @@
     };
   };
 
+  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
   # autoUpgrade for a flake-enabled system
   system.autoUpgrade.flake = ./flake.nix;
+
+  # Printing
+  services.printing.enable = true; # enables printing support via the CUPS daemon
+  services.avahi = {
+    enable = true; # runs the Avahi daemon
+    nssmdns = true; # enables the mDNS NSS plug-in
+    openFirewall = true; # opens the firewall for UDP port 5353
+  };
 
   # Enable GameMode
   programs.gamemode.enable = true;
