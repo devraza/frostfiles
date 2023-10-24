3934e50c9adf033650c7a2492dc2899285257102
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2023-10-24 22:21:36 +0100

    feat: rebase by default

--- home/programs/git/default.nix
+++ home/programs/git/default.nix
@@ -3,10 +3,13 @@
   programs.git = {
     enable = true;
     userName  = "Muhammad Nauman Raza";
-    userEmail = "devraza@outlook.com";
+    userEmail = "devraza@devraza.duckdns.org";
     signing = {
       key = "0xB0EF3A98B29ADB1D";
       signByDefault = true;
+    };
+    extraConfig = {
+      pull.rebase = true;
     };
   };
 }
