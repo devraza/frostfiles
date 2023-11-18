ab72c3b883630f3712722d757ed5b9a63c9e9e79
Muhammad Nauman Raza <devraza@devraza.duckdns.org>
2023-11-18 11:27:49 +0000

    hotfix: override osu!stable wine version

--- home/programs/games/default.nix
+++ home/programs/games/default.nix
@@ -1,10 +1,17 @@
 { pkgs, hostName, inputs, ... }:
 {
-  home.packages = if (hostName == "endogenesis") then 
+  home.packages = if (hostName == "endogenesis") then let
+    gamePkgs = inputs.nix-gaming.packages.${pkgs.hostPlatform.system};
+  in
     with pkgs; [
+      # Override osu!stable wine version
+      (gamePkgs.osu-stable.override rec {
+        wine = pkgs.winePackages.staging;
+        wine-discord-ipc-bridge = gamePkgs.wine-discord-ipc-bridge.override {inherit wine;}; # override the discord-ipc-bridge too
+      })
+  
       steam
-      inputs.nix-gaming.packages.${pkgs.system}.osu-stable
-    ] else
-    with pkgs; [
-    ];
+    ]
+  else
+    with pkgs; [];
 }
