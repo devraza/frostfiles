13e394cc66740057ebb780d2ec4cfd2c5cd89ed2
Muhammad Nauman Raza <devraza@devraza.duckdns.org>
2023-12-09 23:16:15 +0000

    chore: remove aseprite

---
+++ home/programs/games/default.nix
@@ -0,0 +1,24 @@
+{
+  pkgs,
+  hostName,
+  inputs,
+  ...
+}:
+{
+  home.packages = if (hostName == "endogenesis") then let
+    gamePkgs = inputs.nix-gaming.packages.${pkgs.hostPlatform.system};
+  in
+    with pkgs; [
+      # Override osu!stable wine version
+      (gamePkgs.osu-stable.override rec {
+        wine = pkgs.winePackages.staging;
+        wine-discord-ipc-bridge = gamePkgs.wine-discord-ipc-bridge.override {inherit wine;}; # override the discord-ipc-bridge too
+      })
+      steam
+
+      # Game Development
+      # ...
+    ]
+  else
+    with pkgs; [];
+}
