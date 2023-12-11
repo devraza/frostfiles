356a0f5133bd9e241459fda00f3b4d9e42c98158
Muhammad Nauman Raza <devraza@devraza.duckdns.org>
2023-12-11 22:44:53 +0000

    feat: theme for gtk3 apps

--- home/default.nix
+++ home/default.nix
@@ -77,6 +77,10 @@
   # GTK configuration
   gtk = {
     enable = true;
+    theme = {
+      name = "adw-gtk3";
+      package = pkgs.adw-gtk3;
+    };
     iconTheme = {
       name = "Papirus-Dark";
       package = pkgs.papirus-icon-theme;
