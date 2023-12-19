f333fe2a9b8eea9579e593420e9a175f8d31cc99
Muhammad Nauman Raza <devraza@devraza.duckdns.org>
2023-12-19 17:48:09 +0000

    hotfix: curses for icefall

--- home/icefall/services/gpg-agent/default.nix
+++ home/icefall/services/gpg-agent/default.nix
@@ -2,7 +2,7 @@
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
-    pinentryFlavor = "gnome3";
+    pinentryFlavor = "curses";
     sshKeys = [
       "4C00B84E9C63D4ED01382BF739C1AFC7F2454060"
     ];
