fcb795a9351ea7f90ae1005fae253d9f7ad202e8
Muhammad Nauman Raza <devraza@devraza.duckdns.org>
2023-12-24 18:50:56 +0000

    chore: update URL for searxng

--- home/programs/qutebrowser/config.py
+++ home/programs/qutebrowser/config.py
@@ -14,7 +14,7 @@ config.bind('<Shift-k>', 'tab-next')
 c.aliases['mpv'] = 'spawn --userscript view_in_mpv'
 # Define search engines
 c.url.searchengines = {
-    'DEFAULT':  'http://devraza.duckdns.org/search?q={}',
+    'DEFAULT':  'https://search.devraza.duckdns.org/?q={}',
     '!d':       'https://duckduckgo.com/?ia=web&q={}',
     '!gh':      'https://github.com/search?o=desc&q={}&s=stars',
     '!gist':    'https://gist.github.com/search?q={}',
