f74423d6166a0aa4a0d948c9f7e15ba45670881d
Muhammad Nauman Raza <devraza@devraza.duckdns.org>
2023-12-28 23:48:23 +0000

    hotfix: attempt #5

--- home/programs/neovim/lua/core/colorscheme/init.lua
+++ home/programs/neovim/lua/core/colorscheme/init.lua
@@ -2,6 +2,8 @@
 vim.opt.background = 'dark'
 vim.g.colors_name = 'particle'
 
+package.loaded['particle.particle'] = nil
+
 -- include our theme file and pass it to lush to apply
 require('lush')(require('particle.particle'))
 
