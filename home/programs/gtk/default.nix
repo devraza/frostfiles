2c6aa93db5315366f51f3cccfe90e1c42ba5696a
Muhammad Nauman Raza <devraza@devraza.duckdns.org>
2024-01-14 11:30:26 +0000

    chore: do not set gtk theme

--- home/programs/gtk/default.nix
+++ home/programs/gtk/default.nix
@@ -1,9 +1,5 @@
 { pkgs, ... }:
 {
-  # Set the configurations for both gtk3 and gtk4
-  xdg.configFile."gtk-4.0/gtk.css".source = ./gtk-4.0/gtk.css;
-  xdg.configFile."gtk-3.0/gtk.css".source = ./gtk-3.0/gtk.css;
-
   # Misc. GTK configuration
   gtk = {
     enable = true;
