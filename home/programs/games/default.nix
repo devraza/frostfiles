d38ed5629fcc87635e3ea465be1ef551a35f4568
Muhammad Nauman Raza <devraza@devraza.duckdns.org>
2024-01-21 12:29:15 +0000

    chore: remove scanmem

--- home/programs/games/default.nix
+++ home/programs/games/default.nix
@@ -7,9 +7,6 @@
   home.packages = if (hostName == "endogenesis") then with pkgs; [
       steam
       lutris # library manager
-
-      # Game development/hacking
-      scanmem
   ]
   else with pkgs; [];
 }
