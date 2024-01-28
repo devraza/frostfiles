10386252109155653cfea56029a7126b846f3d5d
Muhammad Nauman Raza <devraza@devraza.duckdns.org>
2024-01-28 15:26:43 +0000

    feat: networking improvements via sysctl

--- hosts/icefall/default.nix
+++ hosts/icefall/default.nix
@@ -16,7 +16,26 @@
       device = "/dev/sda";
     };
     supportedFilesystems = [ "ntfs" ];
-    kernel.sysctl."net.ipv4.ip_forward" = 1;
+    # Sysctl values
+    kernel.sysctl = {
+      "net.ipv4.ip_forward" = 1;
+      "net.core.netdev_max_backlog" = 16384;
+      "net.core.somaxconn" = 8192;
+      "net.core.rmem_default" = 1048576;
+      "net.core.rmem_max" = 16777216;
+      "net.core.wmem_default" = 1048576;
+      "net.core.wmem_max" = 16777216;
+      "net.core.optmem_max" = 65536;
+      "net.ipv4.tcp_rmem" = 4096 1048576 2097152;
+      "net.ipv4.tcp_wmem" = 4096 65536 16777216;
+      "net.ipv4.udp_rmem_min" = 8192;
+      "net.ipv4.udp_wmem_min" = 8192;
+      "net.ipv4.tcp_fastopen" = 3;
+      "net.ipv4.tcp_max_syn_backlog" = 8192;
+      "net.ipv4.tcp_max_tw_buckets" = 2097152;
+      "net.ipv4.tcp_tw_reuse" = 1;
+      "net.ipv4.tcp_slow_start_after_idle" = 0;
+    };
   };
 
   nix = {
