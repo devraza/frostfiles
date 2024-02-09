6e64e0854cdb704fd86dee56773094db4b5e71a0
Muhammad Nauman Raza <devraza@devraza.duckdns.org>
2024-02-09 18:58:43 +0000

    feat: cinny-desktop instead of element-desktop

--- home/default.nix
+++ home/default.nix
@@ -16,7 +16,7 @@
       inkscape # svg editor
       gradience # gtk4 theming
       jellyfin-media-player # media player
-      element-desktop # chat
+      cinny-desktop # chat
 
       # Programming
       gcc
