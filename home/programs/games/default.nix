7935a8b0067e8ef0336edc78c234ca9c8cca8723
Muhammad Nauman Raza <devraza@devraza.duckdns.org>
2024-02-14 20:51:25 +0000

    chore: rename gamePkgs

--- home/programs/games/default.nix
+++ home/programs/games/default.nix
@@ -7,14 +7,14 @@
 {
   home.packages =
   if (hostName == "endogenesis") then with pkgs;
-  let gamePkgs = inputs.nix-gaming.packages.${pkgs.hostPlatform.system}; in [
+  let gaming = inputs.nix-gaming.packages.${pkgs.hostPlatform.system}; in [
     steam
     lutris # library manager
 
     wineWowPackages.staging
-    (gamePkgs.osu-stable.override rec {
+    (gaming.osu-stable.override rec {
       wine = wineWowPackages.staging;
-      wine-discord-ipc-bridge = gamePkgs.wine-discord-ipc-bridge.override {inherit wine;};
+      wine-discord-ipc-bridge = gaming.wine-discord-ipc-bridge.override {inherit wine;};
     })
   ]
   else with pkgs; [];
