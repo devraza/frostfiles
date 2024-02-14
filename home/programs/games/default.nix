781487425e45fc2e7b33d09cfa9315a66030a58f
Muhammad Nauman Raza <devraza@devraza.duckdns.org>
2024-02-14 13:51:17 +0000

    hotfix: gamePkgs reference in games

--- home/programs/games/default.nix
+++ home/programs/games/default.nix
@@ -5,12 +5,14 @@
   ...
 }:
 {
-  home.packages = if (hostName == "endogenesis") then with pkgs; [
+  home.packages =
+  if (hostName == "endogenesis") then with pkgs;
+  let gamePkgs = inputs.nix-gaming.packages.${pkgs.hostPlatform.system}; in [
     steam
     lutris # library manager
 
     wineWowPackages.staging
-    (inputs.nix-gaming.packages.${pkgs.hostPlatform.system}.osu-stable.override rec {
+    (gamePkgs.osu-stable.override rec {
       wine = wineWowPackages.staging;
       wine-discord-ipc-bridge = gamePkgs.wine-discord-ipc-bridge.override {inherit wine;};
     })
