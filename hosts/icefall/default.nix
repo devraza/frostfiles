7cfb3bbaff772a1b914ed5edc26c4b272c8ec01e
Muhammad Nauman Raza <devraza@devraza.duckdns.org>
2024-02-20 19:31:51 +0000

    feat: pufferpanel

--- hosts/icefall/default.nix
+++ hosts/icefall/default.nix
@@ -290,6 +290,19 @@
     ];
   };
 
+  # Game server management
+  services.pufferpanel = {
+    enable = true;
+    extraPackages = [ pkgs.jre ];
+    environment = {
+      PUFFER_WEB_HOST = "0.0.0.0:8080";
+      PUFFER_DAEMON_SFTP_HOST = "0.0.0.0:5657";
+      PUFFER_DAEMON_CONSOLE_BUFFER = "1000";
+      PUFFER_DAEMON_CONSOLE_FORWARD = "true";
+      PUFFER_PANEL_REGISTRATIONENABLED = "false";
+    };
+  };
+
   # Media server
   services.jellyfin.enable = true;
 
