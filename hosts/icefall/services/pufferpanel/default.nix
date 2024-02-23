188516eca8fc55fa61ff2cd0bc8376343bf16e3c
Muhammad Nauman Raza <devraza@devraza.duckdns.org>
2024-02-23 13:23:21 +0000

    chore: add missing files

---
+++ hosts/icefall/services/pufferpanel/default.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:
+{
+  # Game server management
+  services.pufferpanel = {
+    enable = true;
+    extraPackages = with pkgs; [
+      temurin-jre-bin-18
+      unzip
+      gnutar
+    ];
+    environment = {
+      PUFFER_WEB_HOST = "0.0.0.0:9291";
+      PUFFER_DAEMON_SFTP_HOST = "0.0.0.0:5657";
+      PUFFER_DAEMON_CONSOLE_BUFFER = "1000";
+      PUFFER_DAEMON_CONSOLE_FORWARD = "true";
+      PUFFER_PANEL_REGISTRATIONENABLED = "false";
+    };
+  };
+}
