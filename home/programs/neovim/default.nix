68ee8685753924bf6546f2dfe937e15888ae16e7
Muhammad Nauman Raza <devraza@devraza.duckdns.org>
2024-03-12 16:09:45 +0000

    feat: all neovim plugins

--- home/programs/neovim/default.nix
+++ home/programs/neovim/default.nix
@@ -2,8 +2,34 @@
 {
   programs.neovim = {
     enable = true;
-    plugins = [
-      pkgs.vimPlugins.nvim-treesitter.withAllGrammars
+    plugins = with pkgs.vimPlugins; [
+      nvim-treesitter.withAllGrammars
+      better-escape-nvim
+      alpha-nvim
+      glow-nvim
+      lush-nvim
+      plenary-nvim
+      nvim-web-devicons
+      neogit
+      diffview-nvim
+      telescope-nvim
+      barbar-nvim
+      lualine-nvim
+      which-key-nvim
+      nvim-lspconfig
+      neorg
+      neorg-telescope
+      lazy-nvim
+      (pkgs.vimUtils.buildVimPlugin {
+        pname = "kagayaki.nvim";
+        version = "2024-02-08";
+        src = pkgs.fetchFromGitHub {
+          owner = "devraza";
+          repo = "kagayaki.nvim";
+          rev = "d91aae17586a7b52b5f8ec6d3f4a514632e4bf22";
+          sha256 = "1bpl8s1bmffiqdh2pwnmcazgl30zkkbl06zvh584wdfs500kjp0g";
+        };
+      })
     ];
   };
 
