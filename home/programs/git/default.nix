fe824c57bae52b292df5b03cd5a39edf60dc8dd5
Muhammad Nauman Raza <devraza@devraza.duckdns.org>
2024-03-23 13:26:18 +0000

    chore: update git email

--- home/programs/git/default.nix
+++ home/programs/git/default.nix
@@ -3,7 +3,7 @@
   programs.git = {
     enable = true;
     userName  = "Muhammad Nauman Raza";
-    userEmail = "devraza@devraza.duckdns.org";
+    userEmail = "devraza@skiff.com";
     signing = {
       key = "0xB0EF3A98B29ADB1D";
       signByDefault = true;
