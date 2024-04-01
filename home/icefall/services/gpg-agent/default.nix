68d17ea467ecbc5705e6d1665ca93768d451d458
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2024-04-01 10:36:29 +0100

    hotfix: pinentryFlavor -> pinentryPackage

--- home/icefall/services/gpg-agent/default.nix
+++ home/icefall/services/gpg-agent/default.nix
@@ -1,8 +1,8 @@
-{
+{ pkgs, ... }: {
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
-    pinentryFlavor = "curses";
+    pinentryPackage = pkgs.pinentry-curses;
     sshKeys = [
       "4C00B84E9C63D4ED01382BF739C1AFC7F2454060"
     ];
--- home/services/gpg-agent/default.nix
+++ home/services/gpg-agent/default.nix
@@ -1,8 +1,8 @@
-{
+{ pkgs, ... }: {
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
-    pinentryFlavor = "gnome3";
+    pinentryPackage = pkgs.pinentry-gnome3;
     sshKeys = [
       "4C00B84E9C63D4ED01382BF739C1AFC7F2454060"
     ];
