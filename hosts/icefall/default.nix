b18ccec162ecb8b0be8a6177d07a0be221c4000b
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2024-04-02 13:30:11 +0100

    feat: `kiwix` systemd service

--- hosts/icefall/default.nix
+++ hosts/icefall/default.nix
@@ -348,18 +348,34 @@
   services.tailscale.enable = true;
 
   # Serve file server
-  systemd.services."dufs" = {
-    script = with pkgs; ''
-      ${dufs}/bin/dufs -A -a devraza:Rl6KSSPbVHV0QHU1@/:rw -b 0.0.0.0 -p 8090 /mnt/codebreaker
-    '';
-    serviceConfig = {
-      type = "simple";
-      user = "root";
+  systemd.services = {
+    "dufs" = {
+      script = with pkgs; ''
+        ${dufs}/bin/dufs -A -a devraza:Rl6KSSPbVHV0QHU1@/:rw -b 0.0.0.0 -p 8090 /mnt/codebreaker
+      '';
+      serviceConfig = {
+        type = "simple";
+        user = "root";
+      };
+      wantedBy = [ "multi-user.target" ];
+      after = [
+        "networkmanager.service"
+        "startup.service"
+      ];
     };
-    wantedBy = [ "multi-user.target" ];
-    after = [
-      "networkmanager.service"
-    ];
+    "kiwix" = {
+      script = with pkgs; ''
+        ${kiwix-tools}/bin/kiwix-serve --port=3920 /mnt/codebreaker/Documents/wikipedia_en_all_maxi_2024-01.zim
+      '';
+      serviceConfig = {
+        type = "simple";
+        user = "root";
+      };
+      wantedBy = [ "multi-user.target" ];
+      after = [
+        "networkmanager.service"
+      ];
+    };
   };
 
   # Matrix configuration
