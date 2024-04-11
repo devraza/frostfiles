b12b1697cc64220835578ac6cd74075351d4d366
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2024-04-11 21:10:34 +0100

    chore: specify identity file in ssh config for home-manager

--- home/programs/ssh/default.nix
+++ home/programs/ssh/default.nix
@@ -4,11 +4,13 @@
     compression = true;
     matchBlocks = {
       "icefall" = {
+        identityFile = "~/.ssh/id_ed25519";
         host = "icefall";
         hostname = "icefall";
         port = 6513;
       };
       "local.icefall" = {
+        identityFile = "~/.ssh/id_ed25519";
         host = "local.icefall";
         hostname = "192.168.1.246";
         port = 6513;
