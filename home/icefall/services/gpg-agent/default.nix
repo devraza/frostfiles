6621afc6d3a73cd0588a48ec56402a00a7dbd805
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2024-04-11 20:44:24 +0100

    chore: remove ssh config

--- home/icefall/services/gpg-agent/default.nix
+++ home/icefall/services/gpg-agent/default.nix
@@ -1,10 +1,6 @@
 { pkgs, ... }: {
   services.gpg-agent = {
     enable = true;
-    enableSshSupport = true;
     pinentryPackage = pkgs.pinentry-curses;
-    sshKeys = [
-      "4C00B84E9C63D4ED01382BF739C1AFC7F2454060"
-    ];
   };
 }
--- home/services/gpg-agent/default.nix
+++ home/services/gpg-agent/default.nix
@@ -1,10 +1,6 @@
 { pkgs, ... }: {
   services.gpg-agent = {
     enable = true;
-    enableSshSupport = true;
     pinentryPackage = pkgs.pinentry-gnome3;
-    sshKeys = [
-      "4C00B84E9C63D4ED01382BF739C1AFC7F2454060"
-    ];
   };
 }
