ff3e1739ceef0617761db4bb80544fb95657a57d
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2024-04-13 18:22:20 +0100

    chore: update mautrix-signal bridge

--- hosts/icefall/services/containers/bridges.nix
+++ hosts/icefall/services/containers/bridges.nix
@@ -1,7 +1,7 @@
 {
   virtualisation.oci-containers.containers = {
     "mautrix-signal" = {
-      image = "dock.mau.dev/mautrix/signal:00f58da3d54e5056082dba3cdc70d59f790a3194-amd64";
+      image = "dock.mau.dev/mautrix/signal:5747acf9d7abe604494cb9c8df5a189fcea6db17-amd64";
       volumes = [
         "/var/lib/mautrix-signal:/data"
       ];
