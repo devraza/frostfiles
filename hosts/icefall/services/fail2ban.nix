738b5bd619e7e52ea6851fcdcd60590862c04cb5
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2024-04-28 10:10:08 +0100

    perf: stricter fail2ban

--- hosts/icefall/services/fail2ban.nix
+++ hosts/icefall/services/fail2ban.nix
@@ -1,12 +1,14 @@
 {
   # Fail2Ban configuration
   environment.etc = {
+    /*
     "fail2ban/filter.d/caddy.conf".text = ''
       [Definition]
       failregex = ^.*"remote_ip":"<HOST>",.*?"status":(?:401|403|500),.*$
       ignoreregex =
       datepattern = LongEpoch
     '';
+    */
     "fail2ban/filter.d/gitea.conf".text = ''
       [Definition]
       failregex =  .*(Failed authentication attempt|invalid credentials|Attempted access of unknown user).* from <HOST>
@@ -24,27 +26,29 @@
     bantime-increment.enable = true;
     ignoreIP = [ "100.64.0.0/24" ];
     jails = {
+      /*
       "caddy" = '' # A maximum of 5 failures in 1 hour
         enabled  = true
         filter   = caddy
-	port     = http,https
-        logpath  = /var/log/caddy/*.access.log
+	      port     = http,https
+        logpath  = /var/log/caddy/*.log
         backend  = auto
         maxretry = 5
         findtime = 3600
       '';
+      */
       "gitea" = ''
         enabled  = true
         filter   = gitea
         logpath  = /var/lib/gitea/log/gitea.log
-        maxretry = 5
+        maxretry = 3
         findtime = 3600
       '';
       "vaultwarden" = ''
         enabled  = true
         filter   = vaultwarden
         logpath  = /var/lib/vaultwarden/vaultwarden.log
-        maxretry = 5
+        maxretry = 3
         findtime = 3600
       '';
     };
