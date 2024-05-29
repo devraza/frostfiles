6f81415ff28d5a90b936dfcf3ef5af365f3bfc4c
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2024-05-29 17:17:35 +0100

    feat: alternate wine version

--- home/programs/games/default.nix
+++ home/programs/games/default.nix
@@ -8,7 +8,8 @@
   home.packages =
   if (hostName == "endogenesis") then with pkgs; [
     lutris # library manager
-    wineWowPackages.staging # windows *games* compat
+    wineWowPackages.staging # windows compat
+    winePackages.staging # alternative windows compat
     osu-lazer-bin # osu!lazer binary
     protonup-ng # install proton-ge
     jdk17 # java
