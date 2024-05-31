a4897d920548b3dab355e247e019dd798b4e80e1
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2024-05-31 17:23:23 +0100

    chore(neovim): delegate plugins to lazy.nvim

--- home/programs/neovim/default.nix
+++ home/programs/neovim/default.nix
@@ -6,36 +6,9 @@
     vimAlias = true;
     plugins = with pkgs.vimPlugins; [
       nvim-treesitter.withAllGrammars
-      better-escape-nvim
-      alpha-nvim
-      glow-nvim
-      lush-nvim
-      plenary-nvim
-      nvim-web-devicons
-      neogit
-      diffview-nvim
-      telescope-nvim
-      barbar-nvim
-      lualine-nvim
-      which-key-nvim
-      nvim-lspconfig
       lazy-nvim
-      nvim-tree-lua
-      toggleterm-nvim
-      typst-vim
-      (pkgs.vimUtils.buildVimPlugin {
-        pname = "kagayaki.nvim";
-        version = "2024-03-26";
-        src = pkgs.fetchFromGitHub {
-          owner = "devraza";
-          repo = "kagayaki.nvim";
-          rev = "49394403a6a279db539e805695b70a706c94ef72";
-          sha256 = "sha256-NLbqedolfQtpoAWMFmixHtBhrD8SWbAsGZ8LXNnR/vU=";
-        };
-      })
     ];
   };
-
   home.packages = with pkgs; [
     gcc
     neovide
