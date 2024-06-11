d672191635a5bcb98067ec384ccbcd98283701a9
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2024-06-11 14:23:24 +0100

    security: disable IPv6 from the kernel parameters

--- shared.nix
+++ shared.nix
@@ -20,7 +20,7 @@
   # Shared kernel + related configuration
   boot = {
     kernelPackages = pkgs.linuxPackages_cachyos;
-    kernelParams = [ "quiet" "splash" "mitigations=off" "intel_pstate=disable" "nowatchdog" "i915.fastboot=1" ];
+    kernelParams = [ "quiet" "splash" "mitigations=off" "intel_pstate=disable" "nowatchdog" "i915.fastboot=1" "ipv6.disable=1" ];
 
     # Clean /tmp on boot, obviously
     tmp.cleanOnBoot = true;
