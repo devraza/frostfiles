c73bf9bd1b5ecf9b7abaf9b5b7e8f8fbc31d919a
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2024-06-11 14:07:04 +0100

    security(endogenesis): assign static IP address

--- hosts/endogenesis/default.nix
+++ hosts/endogenesis/default.nix
@@ -17,6 +17,11 @@
   programs.virt-manager.enable = true;
   virtualisation.libvirtd.enable = true;
 
+  interfaces.wlp3s0.ipv4.addresses = [ {
+    address = "192.168.1.222";
+    prefixLength = 24;
+  } ]; 
+
   # Steam
   programs.steam.enable = true;
 
