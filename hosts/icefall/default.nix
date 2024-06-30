f6fd2cfa5c57e69ce24f0cfd11c86c4d630c4771
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2024-06-30 22:50:30 +0100

    feat: automatic mounting of drive if necessary with weekly backups

--- hosts/icefall/default.nix
+++ hosts/icefall/default.nix
@@ -381,7 +381,7 @@
     "0 4 * * 0 root reboot"
     "0 * * * * devraza . /etc/profile; cd /etc/nixos; ${pkgs.git}/bin/git pull"
     "0 19 * * * root ${pkgs.restic}/bin/restic --repo /var/lib/backup backup /mnt/codebreaker/Documents /var/lib /mnt/codebreaker/Media/Blender /mnt/codebreaker/Media/Books /mnt/codebreaker/Media/Pictures --exclude-file /var/lib/backup/exclude.txt -p /etc/backup.key"
-    "0 1 * * 0 root ${pkgs.restic}/bin/restic --repo /mnt/cosmolight backup /mnt/codebreaker/ -p /etc/cosmolight.key"
+    "0 1 * * 0 root ${pkgs.util-linux}/bin/mountpoint /mnt/codebreaker || ${pkgs.mount}/bin/mount /dev/disk/by-label/cosmolight /mnt/cosmolight && ${pkgs.restic}/bin/restic --repo /mnt/cosmolight backup /mnt/codebreaker/ -p /etc/cosmolight.key"
   ];
 
   # Performance!
