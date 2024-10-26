fbd99000fc04cf63690596c7f58c6b74b7ad8c03
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2024-10-26 22:37:13 +0100

    feat: aagl

--- home/programs/games/default.nix
+++ home/programs/games/default.nix
@@ -4,14 +4,19 @@
   hostName,
   ...
 }:
+let
+  aagl-gtk-on-nix = import (builtins.fetchTarball "https://github.com/ezKEa/aagl-gtk-on-nix/archive/main.tar.gz");
+in
 {
   home.packages = with pkgs; [
     lutris # library manager
     wineWowPackages.staging # windows compat
     protonup-ng # install proton-ge
     jdk21 # java
+    osu-lazer-bin # osu!
     steam
     steam-run # FHS environment
+    aagl-gtk-on-nix.an-anime-game-launcher
   ];
 
   # benchmarking
