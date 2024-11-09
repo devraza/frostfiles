fa31262429c81503a4b059ce15e670453e824a63
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2024-11-09 16:57:07 +0000

    hotfix: use inv-sig-helper to fix invidious

--- hosts/icefall/services/containers/invidious.nix
+++ hosts/icefall/services/containers/invidious.nix
@@ -11,5 +11,17 @@
         "--dns=9.9.9.9"
       ];
     };
+
+    "invidious-helper" = {
+      image = "quay.io/invidious/inv-sig-helper:latest";
+      cmd = ["--tcp" "0.0.0.0:12999"];
+      dependsOn = [ "postgres" ];
+      extraOptions = [
+        "--network=postgres"
+        "--pull=newer"
+        "--dns=9.9.9.9"
+        "--ip=10.89.1.3"
+      ];
+    };
   };
 }
