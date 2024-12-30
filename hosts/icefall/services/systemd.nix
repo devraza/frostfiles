c1d1e01016f4e8d2ebabc89fc231490351d7ae38
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2024-12-30 14:42:45 +0000

    refactor: remove the now obsolete startup service

--- hosts/icefall/services/systemd.nix
+++ hosts/icefall/services/systemd.nix
@@ -6,23 +6,6 @@
 
   # All the custom/modified SystemD services
   systemd.services = {
-    "startup" = {
-      script = with pkgs; ''
-        # Mount the disk
-        sleep 60
-        [ -d /mnt/codebreaker/Documents ] || ${btrfs-progs}/bin/btrfs device scan --all-devices
-        [ -d /mnt/codebreaker/Documents ] || ${mount}/bin/mount /dev/disk/by-label/codebreaker /mnt/codebreaker
-
-        # Restart headscale after some time
-        [ -d /mnt/codebreaker/Documents ] || sleep 600
-        [ -d /mnt/codebreaker/Documents ] || systemctl restart headscale
-      '';
-      serviceConfig = {
-        Type = "oneshot";
-        User = "root";
-      };
-      wantedBy = [ "multi-user.target" ];
-    };
     "podman-qbittorrent-nox" = {
       after = [
         "networkmanager.service"
