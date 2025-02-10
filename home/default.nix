35f3d160eaa3dfccf61d02dc3a727980e73e770f
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2025-02-10 20:55:00 +0000

    hotfix: properly replace `qutebrowser`

--- home/default.nix
+++ home/default.nix
@@ -110,19 +110,6 @@
     settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
   };
 
-  xdg = {
-    mimeApps = {
-      enable = true;
-      defaultApplications = {
-        "text/html" = [ "org.qutebrowser.qutebrowser.desktop" ];
-        "text/xml" = [ "org.qutebrowser.qutebrowser.desktop" ];
-        "x-scheme-handler/http" = [ "org.qutebrowser.qutebrowser.desktop" ];
-        "x-scheme-handler/https" = [ "org.qutebrowser.qutebrowser.desktop" ];
-        "x-scheme-handler/qute" = [ "org.qutebrowser.qutebrowser.desktop" ];
-      };
-    };
-  };
-
   # Protonmail bridge
   systemd.user.services.hydroxide = {
     Install = {
