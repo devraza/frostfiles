35598a929ea80cfcb9aa8dcb3f4bcc96982e0c0a
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2025-08-24 11:58:48 +0100

    feat: remove display of unused desktop entries

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -15,14 +15,42 @@
   ];
 
   xdg.desktopEntries = {
+    "nixos-manual" = {
+      name = "NixOS Manual";
+      noDisplay = true;
+    };
+    "netbird" = {
+      name = "NetBird @ netbird";
+      noDisplay = true;
+    };
+    "cups" = {
+      name = "Manage Printing";
+      noDisplay = true;
+    };
     "fish" = {
       name = "fish";
       exec = "fish";
       noDisplay = true;
     };
-    gammastep-indicator = {
+    "gammastep-indicator" = {
       name = "Gammastep Indicator";
       exec = "gammastep-indicator";
+      noDisplay = true;
+    };
+    "bottom" = {
+      name = "bottom";
+      noDisplay = true;
+    };
+    "yazi" = {
+      name = "Yazi";
+      noDisplay = true;
+    };
+    "rofi" = {
+      name = "Rofi";
+      noDisplay = true;
+    };
+    "rofi-theme-selector" = {
+      name = "Rofi Theme Selector";
       noDisplay = true;
     };
   };
