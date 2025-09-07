47f05967184c698ec2eeb857a485e21ee5c00ace
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2025-09-08 00:30:56 +0100

    chore: remove unused script `dater`

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -1,22 +1,5 @@
 { pkgs, ... }:
-let
-  dater = pkgs.writeShellScriptBin "dater" ''
-    #!/bin/bash
-
-    DaySuffix() {
-      case `date +%D` in
-        1|21|31) echo "st";;
-        2|22)    echo "nd";;
-        3|23)    echo "rd";;
-        *)       echo "th";;
-      esac
-    }
-    date "+%A %e`DaySuffix` %B %Y"
-  '';
-in
 {
   # Import script directories
   imports = [ ./desktop ];
-
-  home.packages = [ dater ];
 }
