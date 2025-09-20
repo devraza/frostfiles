fc8765aa28ac0227d3f73d1ade392c4635c341fd
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2025-09-20 18:00:13 +0100

    chore: remove tinyproxy in favour of netbird routes

--- hosts/icefall/default.nix
+++ hosts/icefall/default.nix
@@ -53,15 +53,6 @@
     usev4 = "webv4, webv4=checkip.dynu.com/, webv4-skip='IP Address'";
   };
 
-  # Proxy for VPN alternative
-  services.tinyproxy = {
-    enable = true;
-    settings = {
-      Port = 5873;
-      Listen = "0.0.0.0";
-    };
-  };
-
   # Automatic upgrades
   system.autoUpgrade = {
     enable = true;
