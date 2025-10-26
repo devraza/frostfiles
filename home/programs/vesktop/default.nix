731250642df4dc8a351e56b0f949a10d8d53ccb2
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2025-10-26 02:07:22 +0000

    hotfix(vesktop): contents of `system24.css`

--- home/programs/vesktop/default.nix
+++ home/programs/vesktop/default.nix
@@ -3,7 +3,7 @@
   programs.vesktop = {
     enable = true;
     vencord.themes = {
-      system24 = "https://refact0r.github.io/system24/build/system24.css";
+      system24 = "@import url('https://refact0r.github.io/system24/build/system24.css');";
     };
     settings.enabledThemes = [ "system24.css" ];
   };
