d218ff9ab2f40e0f88f3130aa3e625a6387e7fd1
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2025-12-09 16:51:15 +0000

    hotfix: mount NAS drive with systemd instead of `/etc/fstab`

--- hosts/icefall/services/systemd.nix
+++ hosts/icefall/services/systemd.nix
@@ -16,4 +16,20 @@
       after = [ "startup.service" ];
     };
   };
+
+  # Various on-boot things
+  systemd.services."startup" = {
+    script = with pkgs; ''
+      # Wait for the dependant services to settle
+      sleep 10
+      # Mount the disk
+      ${mount}/bin/mount /dev/disk/by-uuid/5edfd73b-ffe5-4163-bed3-bb847c62b7fc /mnt/codebreaker
+    '';
+    serviceConfig = {
+      type = "oneshot";
+      user = "root";
+    };
+    wantedBy = [ "multi-user.target" ];
+    after = [ "networkmanager.service" ];
+  };
 }
