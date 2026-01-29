df30525d13dbf6116c11ac9129eea10168644380
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2026-01-29 11:45:21 +0100

    hotfix: modify sysctl values for Tailscale exit node

--- hosts/cryogenesis/default.nix
+++ hosts/cryogenesis/default.nix
@@ -183,6 +183,10 @@
 
   # Overlay mesh network
   services.tailscale.enable = true;
+  boot.kernel.sysctl = {
+   "net.ipv4.ip_forward" = 1;
+   "net.ipv6.conf.all.ip_forward" = 1;
+  };
 
   # Networking
   networking = {
