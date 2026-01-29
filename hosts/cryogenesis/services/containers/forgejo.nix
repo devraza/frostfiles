ddd07b5cbf69aa488c26be6c3adc1e56f7a11b41
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2026-01-29 11:30:49 +0100

    hotfix: use the correct Forgejo docker data path

--- hosts/cryogenesis/services/containers/forgejo.nix
+++ hosts/cryogenesis/services/containers/forgejo.nix
@@ -3,7 +3,7 @@
     containers.forgejo = {
       image = "codeberg.org/forgejo/forgejo:14-rootless";
       volumes = [
-        "/var/lib/git:/data"
+        "/var/lib/git:/var/lib/gitea"
       ];
       extraOptions = [
         "--network=host"
