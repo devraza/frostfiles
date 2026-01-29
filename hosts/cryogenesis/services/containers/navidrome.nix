e0eb8f0a657ef731f29d4038289bbe42aa891d75
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2026-01-30 00:56:16 +0100

    hotfix(navidrome): set correct music folder

--- hosts/cryogenesis/services/containers/navidrome.nix
+++ hosts/cryogenesis/services/containers/navidrome.nix
@@ -4,7 +4,12 @@
     containers = {
       "navidrome" = {
         image = "deluan/navidrome:latest";
-        volumes = [ "/var/lib/navidrome:/data" ];
+        environment = {
+          ND_MUSICFOLDER = "/data/music";
+        };
+        volumes = [
+          "/var/lib/navidrome:/data"
+        ];
         ports = [ "127.0.0.1:4533:4533" ];
         extraOptions = [ "--pull=newer" ];
       };
