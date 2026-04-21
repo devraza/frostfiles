11dbc0958307b708a759f35eff64b501c823e20c
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2026-04-21 13:24:36 +0100

    hotfix: remove supposedly non-existent setting `misc:vfr`

--- home/programs/hyprland/default.nix
+++ home/programs/hyprland/default.nix
@@ -105,7 +105,6 @@
 
       # Miscellaneous
       misc = {
-        vfr = true; # perf
         disable_hyprland_logo = true;
         disable_splash_rendering = true;
         force_default_wallpaper = 0;
